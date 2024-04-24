4a413968482ee37c0ea6f1ba7d8bdd894c53a7bc
Cailey Mocktar <st10262898@vcconnect.edu.za>
2024-04-24 08:57:04 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using CloudDevelopment.Models;
+using Microsoft.AspNetCore.Mvc;
+using ST10262898_CLDV6211_POE.Models;
+using System.Diagnostics;
+
+namespace CloudDevelopment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int userID)
+        {
+            // Retrieve all products from the database
+            List<productTable> products = productTable.GetAllProducts();
+
+            // Pass products and userID to the view
+            ViewData["Products"] = products;
+            ViewData["UserID"] = userID;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult MyWork()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10262898_CLDV6211_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
